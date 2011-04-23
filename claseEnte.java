e180102e78ce944dc1a74391540f63a9afe8e6f6
Marcelo <cherlock.x@gmail.com>
2011-04-22 21:34:49 -0400

    vercion inicial de la tarea 2 de lp

---
+++ claseEnte.java
@@ -0,0 +1,89 @@
+package cl.utfsm.inf.lp;
+import java.util.*;
+import cl.utfsm.inf.lp.Ente;
+
+class claseEnte implements Ente {
+//FALTA HACER EL CONSTRUCTOR PARA INICAILIZAR EL TABLERO Ennnnn 0;
+	private static int tamTablero;
+	private static int[][] tablero = 
+				 new int[tamTablero][tamTablero];
+
+	private	int idTipo;
+	private int rut;
+	private String nombreTipo; 
+	private int[] cord = new int[2];
+	
+	public void setTipo(int tipo){
+		idTipo = tipo;
+		switch (tipo){
+			case 0 :
+				nombreTipo = "Venemo";
+				break;
+			case 1 :
+				nombreTipo = "Elixir";
+				break;
+			case 2 :
+				nombreTipo = "Caballo";
+				break; 
+			case 3 :
+				nombreTipo = "Alfil";
+				break;
+			case 4 : 
+				nombreTipo = "Peon";
+				break;
+		}
+
+	}
+
+	
+
+	public int getTipo(){
+		return idTipo;
+	}
+
+	public String getNombreTipo(){
+		return nombreTipo;
+	}
+
+	public int getId(){
+		return rut;
+	}
+
+	public void setId(int idPass){
+		rut = idPass;
+	}
+	
+	public boolean setPosicion(int x, int y){
+		if ( (x < tamTablero) && (x >= 0 ) &&
+				(y < tamTablero) && (y >= 0 )){ 
+			cord[0] = x;
+			cord[1] = y;		
+			return true;
+		}
+		else 
+			return false;
+	}
+
+	public int[] getPosicion(){
+		return cord;
+	}
+
+}		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
